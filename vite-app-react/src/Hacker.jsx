96d7b611b65b9759e90b7613b3844e9732039281
Andronicus Lepcha <andronicus.lepcha@non.keysight.com>
2024-08-03 19:26:29 +0530

    react basic || vite react || custom react

---
+++ vite-app-react/src/Hacker.jsx
@@ -0,0 +1,10 @@
+function Hacker(){
+    const nameHacker="Androncius"
+    return(
+        <>
+        <h3> Hello Black Hat Hacker {nameHacker} </h3>
+        <p> I am from App.js</p>
+        </>
+    )
+}
+export default Hacker
