7ca5e2750670f94ad9fe1feea117f30a5cb4ebc1
Andronicus Lepcha <andronicus.lepcha@non.keysight.com>
2024-08-06 16:59:47 +0530

    router-react || omponents || loader

---
+++ react-projects/ReactRouter/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-blue-600 p-4'>React Router Project</h1>
+    </>
+  )
+}
+
+export default App
